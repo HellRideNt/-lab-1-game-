402e2ee02c7f102d4cae04451029430d3e47946b
Timofey Cheshegorov <bumbox890@gmail.com>
2026-09-22 18:42:55 +0600

    Добавлен инвентарь. Сделано легкое оформление инвентаря.

--- game.c
+++ game.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define inventory_size 10
 
 // Функция для информации
 
@@ -14,19 +15,65 @@ void info() {
     printf("[0] Завершение программы\n");
     printf("============================================================\n");
     printf("Выберите цифру которая соответсвует вашему запросу:\n");
-    
-
 }
+
+//Список нашего инвентаря======================================================================
+
+    const char* const item_names[10]={
+    "Пусто",
+    "Дерево",
+    "Камень",
+    "Семена",
+    "Меч",
+    "Каменная кирка",
+    "Яблоко",
+    "Факел",
+    "Туалетная бумага",
+    "Вода"
+};
+//=============================================================================================
+
+
+int numbering1;
+int added_time=0;
+int current_day = 1;
+int current_hour = 8;
+int item_id;
+
+//Это значит у нас функция которая выводит слоты===============================================
+void print_inventory(const int inventory[],int size) {
+    printf("================================================================\n");
+    printf("=                          ИНВЕНТАРЬ                           =\n");                      
+    printf("================================================================\n");
+
+    for (int i=0; i < size; i++) {
+    item_id = inventory[i];
+    if (item_id == 0) {
+        printf("Слот %2d: [-] Пусто\n",i+1);
+    } else if (item_id >=1 && item_id <=9) {
+        printf("Слот %2d: [ID: %d] %s\n", i+1, item_id, item_names[item_id]);
+    } else {
+        printf("Cлот %2d: Неизвестный предмет (ID: %d)\n", i+1,item_id);
+    }
+}
+printf("=====================================================================\n");
+}
+
 //====================================================================================================================
+
 int main()
 { 
+int inventory[inventory_size] = {0};
+inventory[0] = 1;
+inventory[1] = 2;
+inventory[2] = 3;
+inventory[3] = 4;
+inventory[4] = 5;
+inventory[5] = 6;
+inventory[6] = 7;
+inventory[7] = 8;
+inventory[8] = 9;
     
-    int numbering1;
-    int added_time=0;
-    int current_day = 1;
-    int current_hour = 8;
-    int inventory = 10;
-
 //======================================================================================================================  
 // Тело Игры 
 while(1) { 
@@ -37,7 +84,7 @@ switch (numbering1) {
 case 1:
     printf("Результат:\n");
     printf("День %d - %d:00 \n",current_day,current_hour);
-    break;
+break;
 case 2:
     printf("На сколько вы бы хотели промотать время\n");
     scanf("%d", &added_time);
@@ -48,28 +95,25 @@ case 2:
         current_day+=1;
         current_hour=current_hour-24;
         printf("День %d - %d:00\n",current_day,current_hour);
-        break;
     }
+break;
+case 3:
+    print_inventory(inventory,inventory_size);
+break;
+
 case 0:
     printf("Игра завершена.\nПока - пока!\n");
     return 0;
 
 default:
-printf("Неверный ввод. Попробуйте снова!\n");
+    printf("Неверный ввод. Попробуйте снова!\n");
 
 }
 }
-
-
-
-
 
 
  return 0; 
 }
-
-
-
 
 
 
