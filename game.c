3fecbe3f22ebe9efd24567f8157782cb7b6adf85
Timofey Cheshegorov <bumbox890@gmail.com>
2026-09-24 20:04:01 +0600

    Разработан 6 пункт - инверсия. Исправлены именования индентификаторов(переменных). Внесены правки в оформление. Программа готова к использованию!

--- game.c
+++ game.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #define inventory_size 10
 // Функция для информации
 
 void info() {
-    printf("============================================================\n");
+    printf("============================МЕНЮ================================\n");
     printf("[1] Выводит на экран текущий день и час\n");
     printf("[2] Промотать время (Поработать)\n");
     printf("[3] Посмотреть инвентарь\n");
     printf("[4] Положить предмет в слот\n");
     printf("[5] Выбросить предмет \n");
-    printf("[6] \n");
+    printf("[6] Инверсия предметов\n");
     printf("[0] Завершение программы\n");
-    printf("============================================================\n");
+    printf("================================================================\n");
     printf("Выберите цифру которая соответсвует вашему запросу:\n");
 }
 
@@ -33,13 +32,12 @@ void info() {
 };
 //=============================================================================================
 
-int numbering1;
-int added_time=0;
+int menu_choice;
+int hours_to_add=0;
 int current_day = 1;
 int current_hour = 8;
 int item_id;
 int index_slot;
-int slot;
 
 //Это значит у нас функция которая выводит слоты===============================================
 void print_inventory(const int inventory[],int size) {
@@ -57,7 +55,7 @@ void print_inventory(const int inventory[],int size) {
         printf("Cлот %2d: [ID:%d] Неизвеcтный предмет\n", i, item_id);
     }
 }
-printf("=====================================================================\n");
+printf("================================================================\n\n\n");
 }
 
 //====================================================================================================================
@@ -79,29 +77,31 @@ inventory[8] = 9;
 while(1) { 
     info();
     
-scanf("%d", &numbering1);
-switch (numbering1) {
+scanf("%d", &menu_choice);
+switch (menu_choice) {
+//==========
 case 1:
     printf("Результат:\n");
-    printf("День %d - %d:00 \n",current_day,current_hour);
+    printf("День %d - %d:00 \n\n\n",current_day,current_hour);
 break;
+//==========
 case 2:
     printf("На сколько вы бы хотели промотать время: \n");
-    if (scanf("%d", &added_time) != 1 || added_time < 0) {
+    if (scanf("%d", &hours_to_add) != 1 || hours_to_add < 0) {
         printf("Ошибка: введите положительное число часов!\n");
         while (getchar() != '\n');
         break;
     }
-    current_hour += added_time;
-
+    current_hour += hours_to_add;
     current_day += current_hour / 24;
     current_hour = current_hour % 24;
-    printf("День %d - %02d:00\n", current_day, current_hour);
+    printf("День %d - %02d:00\n\n\n", current_day, current_hour);
 break;
+//==========
 case 3:
     print_inventory(inventory,inventory_size);
 break;
-
+//==========
 case 4:
     printf("Введите индекс слота (от 0 до %d): \n", inventory_size-1);
     scanf("%d", &index_slot);
@@ -111,9 +111,10 @@ case 4:
         printf("Введите ID предмета: \n");
         scanf("%d", &item_id);
         inventory[index_slot] = item_id;
-        printf("Предмет успешно добавлен в слот %d\n",index_slot);
+        printf("Предмет успешно добавлен в слот %d\n\n\n",index_slot);
     }
 break;
+//==========
 case 5:
     printf("Введите индекс слота предмета, который хотели бы выбросить (от 0 до %d): \n", inventory_size - 1);
     if (scanf("%d", &index_slot) != 1) {
@@ -129,16 +130,30 @@ break;
         printf("Слот %d уже пуст.\n", index_slot);
 break;
     }
-    printf("Предмет с ID %d выброшен из слота %d.\n", inventory[index_slot], index_slot);
-    inventory[index_slot]=0;
+    printf("Предмет с ID %d выброшен из слота %d.\n\n\n", inventory[index_slot], index_slot);
+    inventory[index_slot]=0;   
 break;
-
+//==========
+case 6:
+    printf("===================Инвентарь до переворота======================\n\n");
+    print_inventory(inventory, inventory_size);
+    for (int i = 0; i < inventory_size / 2; i++) {
+        int opposite_index = inventory_size - 1 - i;
+        int temp = inventory[i];
+        inventory[i] = inventory[opposite_index];
+        inventory[opposite_index] = temp;
+    }
+    printf("===================Инвентарь после переворота===================\n\n");
+    print_inventory(inventory, inventory_size);
+    printf("Инвентарь успешно перевернут!\n\n\n");
+break;
+//==========
 case 0:
-    printf("Игра завершена.\nПока - пока!\n");
+    printf("Игра завершена\nПока - пока:)");
     return 0;
-
+//==========
 default:
-    printf("Неверный ввод. Попробуйте снова!\n");
+    printf("Неверный ввод. Попробуйте снова!\n\n\n");
 }
 }
  return 0; 
